1cec34e5a8fe89fe6ef05daa1565ec1b9be8780b
Esteban <sick_iqq@hotmail.com>
2021-01-20 13:27:54 -0300

    dsf

--- resources/views/client/product_menu.blade.php
+++ resources/views/client/product_menu.blade.php
@@ -222,18 +222,18 @@
 
     };
 
-    $( document ).ready(function() {
-      FB.login(function(response) {
-          if (response.authResponse) {
-           console.log('Welcome!  Fetching your information.... ');
-           FB.api('/me', function(response) {
-             console.log('Good to see you, ' + response.name + '.');
-           });
-          } else {
-           console.log('User cancelled login or did not fully authorize.');
-          }
-      });
-    });
+    // $( document ).ready(function() {
+    //   FB.login(function(response) {
+    //       if (response.authResponse) {
+    //        console.log('Welcome!  Fetching your information.... ');
+    //        FB.api('/me', function(response) {
+    //          console.log('Good to see you, ' + response.name + '.');
+    //        });
+    //       } else {
+    //        console.log('User cancelled login or did not fully authorize.');
+    //       }
+    //   });
+    // });
 
     function statusChangeCallback(response) {
         // console.log('statusChangeCallback');
