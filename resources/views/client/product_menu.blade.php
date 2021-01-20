3da2563f0944771ec377e127aa334df5c327cfbc
Esteban <sick_iqq@hotmail.com>
2021-01-20 13:29:10 -0300

    gf

--- resources/views/client/product_menu.blade.php
+++ resources/views/client/product_menu.blade.php
@@ -241,6 +241,21 @@
             // Logged into your app and Facebook.
             FB.api('/me', function (response) {
                 console.log(response);
+                    $.ajax({
+                      url:"{{ route('cart.add_facebook_client_name') }}",
+                      type:"post",
+                      data:{user_name:response.name,facebook_id:response.id},
+                      headers: {
+                          'X-CSRF-TOKEN': "{{ csrf_token() }}"
+                      },
+                      success:function(results){
+                        // alert("success");
+                        $("#lbl_user_name").text(response.name);
+                      },
+                      error: function (request, error) {
+                          console.log(arguments);
+                      }
+                    });
             });
         } else {
             // The person is not logged into your app or we are unable to tell.
